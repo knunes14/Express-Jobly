53923b7e1aa251444023f258ece9b61b0c4e289e
Keegan Nunes <keeganskinner14@gmail.com>
2024-02-18 13:43:28 -0800

    Part one/ first task completed

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,6 +1,19 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/**
+ * Generates SQL query and parameterized values for performing a partial update on a * database table.
+ * This function constructs an SQL UPDATE statement using the provided table name,  * data object, and primary key column name.
+ * 
+ * @param dataToUpdate {Object} {field1: newVal, field2: newVal, ...}
+ * @param jsToSql {Object} maps js-style data fields to database column names,
+ *   like { firstName: "first_name", age: "age" }
+ *
+ * @returns {Object} {sqlSetCols, dataToUpdate}
+ *
+ * @example {firstName: 'Aliya', age: 32} =>
+ *   { setCols: '"first_name"=$1, "age"=$2',
+ *     values: ['Aliya', 32] }
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
---
+++ helpers/sql.test.js
@@ -0,0 +1,31 @@
+const { sqlForPartialUpdate } = require('./sql');
+
+describe('sqlForPartialUpdate', () => {
+  test('Generates correct SQL for single field update', function () {
+    const result = sqlForPartialUpdate(
+        { f1: 'vi' },
+        { f1: 'f1', fF2: 'f2' });
+
+    expect(result).toEqual({
+      setCols: '\"f1\"=$1',
+      values: ['vi'],
+    });
+  });
+
+  test('Generates correct SQL for multiple fields update', function () {
+    const result = sqlForPartialUpdate(
+        { f1: 'vi', jsF2: 'v2' },
+        { jsF2: 'f2' });
+
+    expect(result).toEqual({
+      setCols: '\"f1\"=$1, \"f2\"=$2',
+      values: ['vi', 'v2'],
+    });
+  });
+  
+  test('Throws BadRequestError when no data provided', () => {
+    const dataToUpdate = {};
+    const jsToSql = {};
+    expect(() => sqlForPartialUpdate(dataToUpdate, jsToSql)).toThrowError(BadRequestError);
+  });
+});
